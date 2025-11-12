453b36f7b3c298a0efa8ac7c60444e38566dfd72
Ollie Grooby <hey@grbtd.space>
2025-11-12 00:10:02 +0000

    Added DynamicColorBuilder to follow user device colours

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
 import 'screens/station_list_screen.dart';
 
@@ -8,34 +9,27 @@ void main() {
 class GapMinderApp extends StatelessWidget {
   const GapMinderApp({super.key});
 
+  static final _defaultLightColorScheme = ColorScheme.fromSeed(seedColor: Colors.blueAccent);
+  static final _defaultDarkColorScheme = ColorScheme.fromSeed(seedColor: Colors.blueAccent, brightness: Brightness.dark);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Train Times',
-
-      // Define the light theme based on a seed color
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.blueAccent,
-          brightness: Brightness.light,
-        ),
-        useMaterial3: true,
-      ),
-
-      // Define the dark theme based on a seed color
-      darkTheme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.blueAccent,
-          brightness: Brightness.dark,
-        ),
-        useMaterial3: true,
-      ),
-
-      // Tell Flutter to automatically follow the device's theme
-      themeMode: ThemeMode.system,
-
-      home: const StationListScreen(),
+    return DynamicColorBuilder(
+      builder: (lightColorScheme, darkColorScheme) {
+        return MaterialApp(
+          title: 'GapMinder',
+          theme: ThemeData(
+            colorScheme: lightColorScheme ?? _defaultLightColorScheme,
+            useMaterial3: true,
+          ),
+          darkTheme: ThemeData(
+            colorScheme: darkColorScheme ?? _defaultDarkColorScheme,
+            useMaterial3: true,
+          ),
+          themeMode: ThemeMode.system,
+          home: const StationListScreen(),
+        );
+      },
     );
   }
 }
-
